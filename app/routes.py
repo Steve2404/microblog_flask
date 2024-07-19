3e1441ddd7d67b2debb881b596623764382df781
stevej <jatsasteve@gmail.com>
2024-07-19 06:03:11 +0200

    ajout des fichiers micro microblog.py index.html

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,10 @@
+from flask import render_template
+
 from app import app
+
+
+@app.route('/index')
+@app.route('/')
+def index():
+    user = {'username': 'Bob'}
+    return render_template('index.html', title='Home', user=user)
