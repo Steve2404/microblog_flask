aa85b25c8a4130a63b956226a8a127bfdef5bd13
Steve2404 <jatsasteve@gmail.com>
2024-07-25 17:01:06 +0200

    modification du fichier __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)  # appel des configuration du dossier config.py
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app import routes, models
